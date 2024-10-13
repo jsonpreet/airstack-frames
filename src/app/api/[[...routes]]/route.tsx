135b7bba3cc0c9c0a5a49577c57534ba40aa8f51
MrPreet <josonpreet@gmail.com>
2024-10-13 09:27:25 +0530

    new frames

--- src/app/api/[[...routes]]/route.tsx
+++ src/app/api/[[...routes]]/route.tsx
@@ -4,16 +4,20 @@ import { Button, Frog } from 'frog'
 import { handle } from 'frog/next'
 import { devtools } from 'frog/dev'
 import { serveStatic } from 'frog/serve-static'
-import { neynar } from 'frog/middlewares'
+import { createNeynar } from 'frog/middlewares'
 
-const neynarMiddleware = neynar({
-	apiKey: '7C80F3B5-0FE2-4DAC-B21C-BC6A8684FC3D',
-	features: ['interactor', 'cast'],
-})
+// const neynarMiddleware = neynar({
+// 	apiKey: '7C80F3B5-0FE2-4DAC-B21C-BC6A8684FC3D',
+// 	features: ['interactor', 'cast'],
+// })
 
-const app = new Frog({ title: 'The Professional Fry Slayer Test 🍟', basePath: '/api', browserLocation: '/:path' })
+const neynar = createNeynar({ apiKey: 'NEYNAR_FROG_FM' })
 
-app.frame('/frames/fry-slayer', neynarMiddleware, (c) => {
+const app = new Frog({ title: 'The Professional Fry Slayer Test 🍟', basePath: '/api', browserLocation: '/:path' }).use(
+	neynar.middleware({ features: ['interactor', 'cast'] })
+)
+
+app.frame('/frames/fry-slayer', (c) => {
 	const { buttonValue, status } = c
 	console.log(c.var)
 	const { displayName, followerCount } = c.var.interactor || {}
