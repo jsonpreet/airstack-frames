df85d674d70e2f294cf16d3ece85d8a158ed00ad
MrPreet <josonpreet@gmail.com>
2024-10-13 09:19:11 +0530

    new frames

--- src/app/api/[[...routes]]/route.tsx
+++ src/app/api/[[...routes]]/route.tsx
@@ -7,15 +7,15 @@ import { serveStatic } from 'frog/serve-static'
 import { neynar } from 'frog/middlewares'
 
 const neynarMiddleware = neynar({
-	apiKey: 'NEYNAR_FROG_FM',
+	apiKey: '7C80F3B5-0FE2-4DAC-B21C-BC6A8684FC3D',
 	features: ['interactor', 'cast'],
 })
 
 const app = new Frog({ title: 'The Professional Fry Slayer Test 🍟', basePath: '/api', browserLocation: '/:path' })
 
-app.frame('/', neynarMiddleware, (c) => {
+app.frame('/frames/fry-slayer', neynarMiddleware, (c) => {
 	const { buttonValue, status } = c
-	console.log(c)
+	console.log(c.var)
 	return c.res({
 		image: (
 			<div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
