a3d1c0c3fe410fceb9eb63c93dacb520a5253b6b
MrPreet <josonpreet@gmail.com>
2024-10-26 21:45:24 +0530

    fixing frames 3

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,32 @@
 import { fetchMetadata } from 'frames.js/next'
 
 export async function generateMetadata() {
-	try {
-		const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
-		console.log('Fetching metadata from:', new URL('/frames', baseUrl).toString())
-		const frameMetadata = await fetchMetadata(new URL('/frames', baseUrl))
-		console.log('Fetched metadata:', frameMetadata)
-		return {
-			title: 'Jessy Frame',
-			other: {
-				...frameMetadata,
-			},
-		}
-	} catch (error) {
-		console.error('Error fetching metadata:', error)
-		return {
-			title: 'Jessy Frame',
-			other: {
-				// Fallback metadata
-			},
-		}
+	return {
+		title: 'Jessy Frame',
+		other: {
+			// Fallback metadata
+		},
 	}
+	// try {
+	// 	const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
+	// 	console.log('Fetching metadata from:', new URL('/frames', baseUrl).toString())
+	// 	const frameMetadata = await fetchMetadata(new URL('/frames', baseUrl))
+	// 	console.log('Fetched metadata:', frameMetadata)
+	// 	return {
+	// 		title: 'Jessy Frame',
+	// 		other: {
+	// 			...frameMetadata,
+	// 		},
+	// 	}
+	// } catch (error) {
+	// 	console.error('Error fetching metadata:', error)
+	// 	return {
+	// 		title: 'Jessy Frame',
+	// 		other: {
+	// 			// Fallback metadata
+	// 		},
+	// 	}
+	// }
 }
 
 export default function Page() {
